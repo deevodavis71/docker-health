d012a123c5f4a2d242da7e19050b1aa8aafd7cf7
Steve Davis <deevodavis71@gmail.com>
2017-09-08 17:44:57 +0100

    First commit

---
+++ src/main/java/com/sjd/dockerhealth/DockerHealthApplication.java
@@ -0,0 +1,12 @@
+package com.sjd.dockerhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerHealthApplication.class, args);
+	}
+}
